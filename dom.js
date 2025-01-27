5de9a31d6936f20ebc90b13988f715de7d1cdf2d
Ashwikanjani29 <ashwikanjanivalasala2910@gmail.com>
2025-01-27 12:55:46 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -88,21 +88,83 @@
 // 
 
 
-let form = document.querySelector("form");
-let name=document.getElementById("uName");
-let email=document.getElementsById("uEmail");
-let password=document.getElementById("uPassword");
+// let form = document.querySelector("form");
+// let name=document.getElementById("uName");
+// let email=document.getElementsById("uEmail");
+// let password=document.getElementById("uPassword");
 
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    // console.log(event);
-    // console.lof("submitted");
-    let uName =name.value;
-    let uEmail = email.value;
-    let uPassword=password.value;
-    let userDetails={
-        uName,uEmail,uPassword
-    }
-    console.log(userDetails);
-    localStorage("userData",JSON.stringify(userDetails))
-})
+// form.addEventListener("submit",(event)=>{
+//     event.preventDefault();
+//     // console.log(event);
+//     // console.lof("submitted");
+//     let uName =name.value;
+//     let uEmail = email.value;
+//     let uPassword=password.value;
+//     let userDetails={
+//         uName,uEmail,uPassword
+//     }
+//     console.log(userDetails);
+//     localStorage("userData",JSON.stringify(userDetails))
+// })
+
+
+// let video = document.createElement("video");
+// video.setAttribute("src", "./wallpaper.jpg")
+// console.log(video);
+// video.setAttribute("controls","controls");
+
+// document.body.appendChild(video);
+
+
+// let h1 =document.createElement("h1");
+// h1.setAttribute("id","demo");
+// h1.innerText="hello";
+// console.log(h1);
+// document.body.appendChild(h1);
+
+
+
+let mainEle = document.createElement("div");
+mainEle.setAttribute("id","mainBlock");
+
+mainEle.style.border="2px solid red";
+mainEle.style.width="600px";
+mainEle.style.height="500px";
+
+console.log(mainEle);
+
+let topEle = document.createElement("div");
+topEle.setAttribute("id","topBlock");
+
+let image=document.createElement("img");
+image.src="./wallpaper 1.jpg";
+image.style.width="600px";
+image.style.height="600px"
+// console.log(topEle);
+
+
+let bottomEle= document.createElement("div");
+bottomEle.setAttribute("id","bottomBlock");
+// console.log(bottomEle);
+
+let h= document.createElement("button");
+h.innerText="cat";
+h.style.textAlign="center";
+
+let btn=document.createElement("button");
+btn.innerText="view more";
+btn.style.border="none";
+btn.style.padding="5px";
+btn.style.backgroundColor="dodgerblue";
+btn.style.color="white";
+
+
+
+bottomEle.appendChild(h);
+bottomEle.appendChild(btn);
+
+topEle.append(image);
+mainEle.appendChild(topEle);
+mainEle.appendChild(bottomEle);
+
+document.body.appendChild(mainEle);
