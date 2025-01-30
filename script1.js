f37bb313d20bbd29e66ddecf7844b1dfa1db85ac
Ashwikanjani29 <ashwikanjanivalasala2910@gmail.com>
2025-01-30 12:50:08 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+export const c="mru";
+const marks=100;
+const m1 ="pass";
+export function tasking(){
+    console.log("i am at "+c);
+    document.getElementById("hello").innerText="good"
+}
+// export {c,tasking};
+export default {marks,m1};
---
+++ script2.js
@@ -0,0 +1,4 @@
+import {c}from "./script1.js";
+import {tasking } from "./script1.js";
+tasking();
+console.log(total);
