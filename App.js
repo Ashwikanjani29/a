95d252ee7fffb02491d8d5d9b3cfa4ce50fa87fb
Ashwikanjani29 <ashwikanjanivalasala2910@gmail.com>
2025-02-03 13:00:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// function App() {
+//   // let e= React.createElement("div",{className:"App"}, 
+//   //   React.createElement("h1",null,"hi"))
+//   let u= "aaa";
+//   return (
+//     // <div className="App">
+//     //   <h1>hello</h1>
+//     // </div>
+//     // <section>
+//     //   <h1>hello</h1>
+//     // </section>
+//     <div>
+//     <h1>{u}</h1>
+//     <p>{100-9}</p>
+//     </div>
+    
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <h1 style={{color:"red"}}>One hour wasted</h1>
-    </div>
-  );
+// export default App;
+
+//cbc
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <h1>hi</h1>
+//     )
+//   }
+// }
+// export default App();
+
+//fbc
+function App(){
+  return(
+    <h1>hi</h1>
+  )
 }
-
-export default App;
+export default App();
