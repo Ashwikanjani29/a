d5ca56e3ef34422ed7e9a67a1f378e8c54209420
Ashwikanjani29 <ashwikanjanivalasala2910@gmail.com>
2025-02-04 13:01:38 +0530

    Add files via upload

---
+++ propex/CBCPropex.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CBCPropex extends Component {
+  render() {
+    console.log(this);
+    return (
+      <div>CBCPropex
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
